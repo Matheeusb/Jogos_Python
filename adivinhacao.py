b6d278126737e27304f6bb585d930164bc146671
Matheus Schroder Bereta <matheus_bereta@sicredi.com.br>
2019-02-26 16:40:25 -0300

    Ajustes

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,17 +1,17 @@
 import random
 
 print("*************************************")
-print("Ol√°, bem vindo ao jogo de adivinha√ß√£o")
+print("Ol·, bem vindo ao jogo de adivinhaÁ„o")
 print("*************************************")
 
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 0
 pontos = 1000
 
-print("Qual n√≠vel de dificuldade?")
-print("(1) F√°cil (2) M√©dio (3) Dif√≠cil")
+print("Qual nÌvel de dificuldade?")
+print("(1) F·cil (2) MÈdio (3) DifÌcil")
 
-nivel = int(input("Defina o n√≠vel: "))
+nivel = int(input("Defina o nÌvel: "))
 
 if nivel == 1:
     total_de_tentativas = 10
@@ -20,19 +20,19 @@ elif nivel == 2:
 elif nivel == 3:
     total_de_tentativas = 3
 else:
-    print("N√≠vel escolhido n√£o existe!")
-
+    print("NÌvel escolhido n„o existe!")
+ 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_srt = input("Digite o seu n√∫mero: ")
+    chute_srt = input("Digite o seu n˙mero: ")
     chute = int(chute_srt)
 
     if chute < 0 or chute > 100:
-        print("Voc√™ deve digitar um n√∫mero entre 0 e 100")
+        print("VocÍ deve digitar um n˙mero entre 0 e 100")
         continue
 
     if numero_secreto == chute:
-        print("Voc√™ acertou e fez {} pontos".format(pontos))
+        print("VocÍ acertou e fez {} pontos".format(pontos))
         break
     else:
         if chute > numero_secreto:
