110c6cb4a265bcc8e32b3a19c452f25fbe1f1940
Matheus Schroder Bereta <matheus_bereta@sicredi.com.br>
2019-03-19 08:34:29 -0300

    Avançando com o jogo da forca!

--- forca.py
+++ forca.py
@@ -1,8 +1,28 @@
-
-
 def jogar():
     print("*******************************")
     print("Olá, bem vindo ao jogo de forca")
     print("*******************************")
 
+    palavra_secreta = "python"
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}.".format(chute, index))
+            index = index + 1
+
+        print("Jogando...")
+
     print("Fim do jogo!")
+
+
+if __name__ == "__main__":
+    jogar()
